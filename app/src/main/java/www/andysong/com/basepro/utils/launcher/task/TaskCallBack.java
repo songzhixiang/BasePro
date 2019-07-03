a81fc0314400037ecf67c7d1c17129291010c4f0
AndySong <songzhixiang960425@outlook.com>
2019-07-03 17:41:09 +0800

    commit 异步加载任务功能，以及加载算法

---
+++ app/src/main/java/www/andysong/com/basepro/utils/launcher/task/TaskCallBack.java
@@ -0,0 +1,10 @@
+package www.andysong.com.basepro.utils.launcher.task;
+
+/**
+ * @author andysong
+ * @data 2019-07-03
+ * @discription xxx
+ */
+public interface TaskCallBack {
+    void call();
+}
